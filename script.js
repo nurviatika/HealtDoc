f038ff3a9955fcb36a8fed884f33c5d8a9542991
siti nurviatika <sitinurviatika@gmail.com>
2025-02-25 23:16:57 +0700

    Menambahkan dan memperbarui file

--- script.js
+++ script.js
@@ -2,12 +2,60 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     const navLinks = document.querySelector(".nav-links");
     const spans = menuToggle.querySelectorAll("span");
+    if (menuToggle && navLinks) {
+        menuToggle.addEventListener("click", function () {
+            navLinks.classList.toggle("active");
+            spans[0].classList.toggle("rotate-down");
+            spans[1].classList.toggle("hide");
+            spans[2].classList.toggle("rotate-up");
+        });
+    }
 
-    menuToggle.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
+    var swiperFrame = new Swiper(".frame-image", {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        loop: true,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false, 
+        },
+        speed: 1000, 
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            375: { slidesPerView: 2, spaceBetween: 15 },
+            640: { slidesPerView: 3, spaceBetween: 10 },
+            768: { slidesPerView: 3, spaceBetween: 10 },
+            1024: { slidesPerView: 4, spaceBetween: 15 },
+        }
+    });
 
-        spans[0].classList.toggle("rotate-down");
-        spans[1].classList.toggle("hide");
-        spans[2].classList.toggle("rotate-up");
+    var swiperFeatures = new Swiper(".features-image", {
+        loop: true,
+        slidesPerView: 1, 
+        spaceBetween: 20,
+        autoplay: {
+            delay: 2000, 
+            disableOnInteraction: false, 
+        },
+        speed: 1000,
+        effect: "slide", 
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            576: { slidesPerView: 1, spaceBetween: 10 },
+        }
     });
 });
